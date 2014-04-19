1ddf6f8daa2671b4d329176a939b0483fcae8e31
Landon <meernik@live.com>
2014-04-19 05:40:51 +0000

    Did some stuff. not sure if it works

--- Neuron.py
+++ Neuron.py
@@ -16,26 +16,68 @@ ZO RELAXEN UND WATSCHEN DER BLINKENLICHTEN.
 #Original code from Byron Galbraith via neurdon
 
 
-from __future__ import division
-from numpy import *
 import numpy as np
-#from pylab import *
-from scipy import *
+import scipy as sp
 import sys
+import uuid
+
+class Model(object):
+    def __init__(self, dT):
+        self._dT = dT
+        self._neurons = {}
+        self._synapses = []
+        self._key_order = []
+        self._t = 0.
+    def add(self, **kwargs):
+        n = Neuron(**kwargs)
+        k = uuid.uuid4()
+        self._neurons[k] = n
+        self._key_order.append(k)
+        return (k, n)
+    def header(self):
+        return self._key_order;
+    def connect(self, prekey, postkey, weight):
+        s = Synapse(self._neurons[prekey], self._neurons[postkey])
+        self._synapses.append(s)
+        return s
+    def step(self):
+        v = [self._t]
+        self._t += self._dT
+        for k in self._key_order:
+            v.append(self._neurons[k].step(self._dT))
+        return v
+
+class Synapse(object):
+    def __init__(self, pre, post, weight=None):
+        self.weight = weight if weight is None else np.ranf()
+        self.pre = pre
+        self.post.inputs.append(self)
+    def transfer(self):
+        "Effect on postsynaptic current"
+        return pre.history[-1].V * self.weight
 
 class Neuron(object):
     def alpha_n(self,v):
-        return 0.01*(-v + 10)/(exp((-v + 10)/10) - 1) if v != 10 else 0.1
+        return 0.01*(-v + 10)/(sp.exp((-v + 10)/10) - 1) if v != 10 else 0.1
     def beta_n(self,v):
-        return 0.125*exp(-v/80)
+        return 0.125*sp.exp(-v/80)
     def alpha_m(self,v):
-        return 0.1*(-v + 25)/(exp((-v + 25)/10) - 1) if v != 25 else 1
+        return 0.1*(-v + 25)/(sp.exp((-v + 25)/10) - 1) if v != 25 else 1
     def beta_m(self,v):
-        return 4*exp(-v/18)
+        return 4*sp.exp(-v/18)
     def alpha_h(self,v):
-        return 0.07*exp(-v/20)
+        return 0.07*sp.exp(-v/20)
     def beta_h(self,v):
-        return 1/(exp((-v + 30)/10) + 1)
+        return 1/(sp.exp((-v + 30)/10) + 1)
+
+    class _histent(object):
+        def __init__(self, V, m, n, h, I):
+            self.V = V
+            self.m = m
+            self.n = n
+            self.h = h
+            self.I = I
+
     def __init__(
         self,
         ### channel activity ###
@@ -60,26 +102,35 @@ class Neuron(object):
         self.E_Na   = E_Na
         self.E_K    = E_K
         self.E_l    = E_l
-        self.I      = I
+        self._I     = I
+        self.I      = 0
         # Initial Conditions
         self.V      = V_zero
         self.m      = self.alpha_m(V_zero)/(self.alpha_m(V_zero) + self.beta_m(V_zero))
         self.n      = self.alpha_n(V_zero)/(self.alpha_n(V_zero) + self.beta_n(V_zero))
         self.h      = self.alpha_h(V_zero)/(self.alpha_h(V_zero) + self.beta_h(V_zero))
+        # idx n = value at T - n * dT
+        self.inputs  = []
+        self.histlen = 10
+        self.history = [self._histent(self.V, self.m, self.n, self.h, self.I) for _ in range(self.histlen)]
 
     def __str__(self):
         return "HH Neuron: m: \t%r\tn:%r\th:\t%r\tV:\t%r" % (
                self.m, self.n, self.h, self.V)
     def step(self, dT):
-        g_Na = self.gbar_Na*(self.m**3)*self.h
-        g_K  = self.gbar_K*(self.n**4)
-        g_l  = self.gbar_l  
+        "Step the model by dT. Strange things may happen if you vary dT"
+        self.I = sum([i.transfer() for i in self.inputs])
 
         self.m += dT*(self.alpha_m(self.V)*(1 - self.m) - self.beta_m(self.V)*self.m)
         self.h += dT*(self.alpha_h(self.V)*(1 - self.h) - self.beta_h(self.V)*self.h)
         self.n += dT*(self.alpha_n(self.V)*(1 - self.n) - self.beta_n(self.V)*self.n)
-        self.V += (self.I - 
+        g_Na = self.gbar_Na*(self.m**3)*self.h
+        g_K  = self.gbar_K*(self.n**4)
+        g_l  = self.gbar_l 
+        self.V += (self._I + self.I - 
                     g_Na*(self.V - self.E_Na) - 
                     g_K*(self.V - self.E_K) - 
                     g_l*(self.V- self.E_l)) / self.Cm * dT
+        self.history.append(self._histent(self.V, self.m, self.n, self.h, self.I))
+        del self.history[0]
         return self.V
--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python2.7
 
-from Neuron import Neuron
+from Neuron import Model, Neuron
 import sys
 import argparse
 import csv
 import BaseHTTPServer
 import urlparse
+import uuid
+import json
 
 def main(steps=1000, dT=.025, output=sys.stdout, **kwargs):
     """Serve doesn't do anything here, but filters the arg out for Neuron()"""
@@ -14,7 +16,6 @@ def main(steps=1000, dT=.025, output=sys.stdout, **kwargs):
     b = Neuron(I=15);
     V = [a.step(dT) for _ in xrange(steps)]
     writer = csv.writer(output)
-    writer.writerow(["T", "V"])
     writer.writerows(zip((i * dT for i in xrange(len(V))), V))
 
 def try_parse(thing):
@@ -29,27 +30,40 @@ def try_parse(thing):
         pass
     return thing
 
+model = None
+
 class SARequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     """
     Calls main() with output=(request wfile), and **kwargs=GET/POST parameters.
     You should probably not deploy this on the actual internet.
     """
     def do_GET(self):
+        global model
         res = urlparse.urlparse(self.path)
         params = { # urlparse passes EVERYTHING back as a list, this extracts singletons.
             key : try_parse(val[0]) if len(val) == 1 else val
             for key, val in urlparse.parse_qs(res.query).iteritems()
         }
         print("got a GET request for %r, params %r" % (res, params))
-        path = res.path.split("/")
-        
-        writer = csv.writer(self.wfile)
+        # Empty strings are lame
+        path = [pe for pe in res.path.strip().split("/") if pe is not ""]
         self.send_response(200)
         self.send_header('Content-Type', "text/plain")
         self.end_headers()
-        main(output=self.wfile, **params)
-        print("Finished request for %s" % self.path)
-        
+        if path[0] == 'one':
+            writer = csv.writer(self.wfile)
+            main(output=self.wfile, **params)
+        elif path[0] == 'new':
+            k, n = model.add(**params)
+            self.wfile.write(k)
+            print "Added %s" % k
+        elif path[0] == 'step':
+            print "Attempting to iterate %r steps" % params['steps']
+            V = [model.step() for step in range(params['steps'])]
+            json.dump(V, self.wfile)
+            print "Stepped"
+        print("Finished request for %r" % path)
+
     def do_POST(self):
         print("got a POST request")
         print(self.rfile.read())
@@ -70,9 +84,10 @@ if __name__ == "__main__":
     parser.add_argument(      '--E_Na',                     metavar="mV",       type=float, action="store")
     parser.add_argument(      '--E_K',                      metavar="mV",       type=float, action="store")
     parser.add_argument(      '--E_l',                      metavar="mV",       type=float, action="store")
-    parser.add_argument(      '--I',                        metavar="mA",       type=float, action="store")
+    parser.add_argument(      '--I',                        metavar="mA?",      type=float, action="store")
     args = parser.parse_args()
     if args.serve:
+        model = Model(args.dT)
         server = BaseHTTPServer.HTTPServer(('', 3103), SARequestHandler)
         print "Giving up control flow!"
         server.serve_forever()
