828884be6e53a971a713faddd434881239278b7d
Steven Lockhart <steven99lockhart@gmail.com>
2014-08-14 13:49:22 -0700

    Add debug logging to new Arduino Code

--- arduino_controller_reborn/arduino_controller_reborn.ino
+++ arduino_controller_reborn/arduino_controller_reborn.ino
@@ -4,6 +4,13 @@
 #include <EthernetUdp.h>
 #include <SPI.h>
 
+//#define DEBUG // Uncomment for debug()
+#ifdef DEBUG
+#define debug(x) Serial.print(x)
+#else
+#define debug(x)
+#endif
+
 #define LED_PIN 22
 
 #define NUM_LEDS 1000  // TODO: Get final number of LEDs
@@ -55,6 +62,8 @@ void set_frame_solid_color(byte color[]) {
 void setup() {
   byte test_color[3];
   
+  debug("Starting test\n");
+  
   /* Initialize frame buffer and test LEDs */
   test_color = { 0xFF, 0x00, 0x00 };
   set_frame_solid_color(test_color);
@@ -65,6 +74,9 @@ void setup() {
   test_color = { 0x00, 0x00, 0xFF };
   set_frame_solid_color(test_color);
   delay(1000);
+
+  debug("Test Complete\n");
+  debug("Listening for packets\n");
 
   /* Start listening for packets */
   Ethernet.begin(mac, ip);
@@ -81,15 +93,35 @@ void loop() {
     /* If there's a packet available at all, read it out of the receive buffer into the packet
        buffer */
     udp.read(packet_buffer, MAX_PACKET_SIZE);
+    debug("Received a packet of size ");
+    debug(packet_size);
+    debug("\n");
     if (packet_size == MAX_PACKET_SIZE) {
       // If the packet is complete, its first byte is which section of the frame to update
       packet_frame_section = packet_buffer[0];
       // Make sure the frame section is valid so we don't segfault
       if (packet_frame_section < PACKETS_PER_FRAME) {
+        debug("Updating frame_section ");
+        debug(packet_frame_section);
+        debug("\n");
         memcpy(frame_buffer + (MAX_PACKET_SIZE * packet_frame_section),
             packet_buffer + 1, packet_size);
-      } // else drop invalid packet
-    } // else drop incomplete packet
+      } else {
+        // Drop invalid packet
+        debug("Dropping invalid packet, packet_frame_section = ");
+        debug(packet_frame_section);
+        debug(", PACKETS_PER_FRAME = ");
+        debug(PACKETS_PER_FRAME);
+        debug("\n");
+      }
+    } else {
+      // Drop incomplete packet
+      debug("Dropping incomplete packet, packet_size = ");
+      debug(packet_size);
+      debug(", MAX_PACKET_SIZE = ");
+      debug(MAX_PACKET_SIZE);
+      debug("\n");
+    }
   }
   
   update_leds();
