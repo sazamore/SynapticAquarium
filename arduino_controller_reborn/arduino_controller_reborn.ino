98ae4252ea61abe5c47423bab1f31e39f46c261b
Steven Lockhart <steven99lockhart@gmail.com>
2014-08-17 18:40:06 -0700

    Final Except Optimizations

--- arduino_controller_reborn/arduino_controller_reborn.ino
+++ arduino_controller_reborn/arduino_controller_reborn.ino
@@ -1,5 +1,5 @@
 #include <Adafruit_NeoPixel.h>
-
+#define W5200
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 #include <SPI.h>
@@ -11,13 +11,18 @@
 #define debug(x)
 #endif
 
+//#define SUPER_DEBUG // Uncomment for debug()
+#ifdef SUPER_DEBUG
+#define super_debug(x) Serial.print(x)
+#else
+#define super_debug(x)
+#endif
+
 #define LED_PIN 22
 
-#define NUM_LEDS 1000  // TODO: Get final number of LEDs
-#define PACKETS_PER_FRAME 3  // TODO: Update once we have the final number of LEDs
-                             /* Should be a small integer that evenly divides NUM_LEDs (when
-                                rounded up) */
-#define MAX_PACKET_SIZE (NUM_LEDS * 3 / PACKETS_PER_FRAME) + 1  // 1001 bytes
+/* packet size and the number of LEDs are set by the model */
+#define NUM_LEDS 1233  // TODO: Get final number of LEDs
+#define MAX_PACKET_SIZE 512
 
 /*
  * Setup the one LED strip:
@@ -33,13 +38,15 @@
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_RGB + NEO_KHZ800);
 
 /* Network setup */
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0x00, 0x00};
-IPAddress ip(10, 10, 32, 1);
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEF};
+byte ip[] = {10, 10, 32, 1};
+byte subnet[] = {255, 255, 255, 0};
+byte gateway[] = {10, 10, 32, 0};
 unsigned int port = 9999;
-EthernetUDP udp;
+EthernetServer server(port);
 
 char packet_buffer[MAX_PACKET_SIZE];
-char frame_buffer[NUM_LEDS * 3];
+char frame_buffer[4096];
 
 /* Output the frame_buffer to the LEDs and refresh the LEDs */
 void update_leds() {
@@ -54,75 +61,73 @@ void update_leds() {
 /* Set every LED to a single color */
 void set_frame_solid_color(byte color[]) {
   for (int i = 0; i < NUM_LEDS; i++) {
-    memcpy(frame_buffer + i, color, 3);
+    memcpy(frame_buffer + (i * 3), color, 3);
   }
   update_leds();
 }
 
 void setup() {
   byte test_color[3];
-  
+   
+  #ifdef DEBUG
+    Serial.begin(9600);
+  #endif
+
   debug("Starting test\n");
-  
+
   /* Initialize frame buffer and test LEDs */
-  test_color = { 0xFF, 0x00, 0x00 };
+  strip.begin(); 
+  test_color = { 0x44, 0x00, 0x00 };
+  set_frame_soliq  d_color(test_color);
+  delay(1000);
+  test_color = { 0x00, 0x44, 0x00 };
   set_frame_solid_color(test_color);
   delay(1000);
-  test_color = { 0x00, 0xFF, 0x00 };
+  test_color = { 0x00, 0x00, 0x44 };
   set_frame_solid_color(test_color);
-  delay(1000);
-  test_color = { 0x00, 0x00, 0xFF };
+  delay(3000);
+  test_color = { 0x00, 0x00, 0x00 };
   set_frame_solid_color(test_color);
-  delay(1000);
 
   debug("Test Complete\n");
   debug("Listening for packets\n");
 
   /* Start listening for packets */
   Ethernet.begin(mac, ip);
-  udp.begin(port);
+  server.begin();
 }
 
 void loop() {
   int packet_size = 0;
   int packet_frame_section = 0;
 
-  /* Update the frame buffer */
-  packet_size = udp.parsePacket();
-  if (packet_size > 0) {
-    /* If there's a packet available at all, read it out of the receive buffer into the packet
-       buffer */
-    udp.read(packet_buffer, MAX_PACKET_SIZE);
+  // Create a client TCP connection
+  EthernetClient client = server.available();
+  if (client && client.connected() and client.available()) {
+    packet_size = client.read((uint8_t *)packet_buffer, MAX_PACKET_SIZE + 1);
+    packet_frame_section = packet_buffer[0];
     debug("Received a packet of size ");
     debug(packet_size);
-    debug("\n");
-    if (packet_size == MAX_PACKET_SIZE) {
-      // If the packet is complete, its first byte is which section of the frame to update
-      packet_frame_section = packet_buffer[0];
-      // Make sure the frame section is valid so we don't segfault
-      if (packet_frame_section < PACKETS_PER_FRAME) {
-        debug("Updating frame_section ");
-        debug(packet_frame_section);
-        debug("\n");
-        memcpy(frame_buffer + (MAX_PACKET_SIZE * packet_frame_section),
-            packet_buffer + 1, packet_size);
-      } else {
-        // Drop invalid packet
-        debug("Dropping invalid packet, packet_frame_section = ");
-        debug(packet_frame_section);
-        debug(", PACKETS_PER_FRAME = ");
-        debug(PACKETS_PER_FRAME);
-        debug("\n");
-      }
-    } else {
-      // Drop incomplete packet
-      debug("Dropping incomplete packet, packet_size = ");
-      debug(packet_size);
-      debug(", MAX_PACKET_SIZE = ");
-      debug(MAX_PACKET_SIZE);
-      debug("\n");
+    super_debug(".\n");
+    super_debug("\tFirst eight bytes of packet = [");
+    for (int i = 0; i < 8; i++) {
+      super_debug(packet_buffer[i]);
+      super_debug(", ");
     }
+    debug("]\n");
+    debug("Updating frame_section ");
+    debug(packet_frame_section);
+    debug(" at frame_buffer offset = ");
+    debug(MAX_PACKET_SIZE * packet_frame_section);
+    debug("\n");
+    memcpy(frame_buffer + (MAX_PACKET_SIZE * packet_frame_section),
+      packet_buffer + 1, packet_size);
+    super_debug("\tFirst eight bytes of updated part = [");
+    for (int i = 0; i < 8; i++) {
+      super_debug(frame_buffer[(MAX_PACKET_SIZE * packet_frame_section) + i]);
+      super_debug(", ");
+    }
+    super_debug("]\n");
+    update_leds();
   }
-  
-  update_leds();
 }
